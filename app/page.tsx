e1cb9040860ccbfbe17a1ae4775d906912e4f47a
ShubhamSharma2311 <sharma.2022ug2068@iiitranchi.ac.in>
2025-05-09 14:53:03 +0530

    fixing bugs

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HowItWorks from './components/HowItWorks';
 import WhyChooseUs from './components/WhyChooseUs';
-import Testimonials from './components/Testimonials';
+import Testimonials from './components/testimonials';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 
